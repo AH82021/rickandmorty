42aba75fe66e3a96e92c8d48738451921bdc174d
Ajmal Hakim <samimajmal@yahoo.com>
2024-01-30 14:44:11 -0500

    rick and morty api with react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import CharacterList from "./component/CharacterList"
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <>
+    <h1 className="app-title"> Rick And Morty</h1>
+     <CharacterList />
+    </>
+  )
+}
+
+export default App
+/// promise 
+// hooks 
+// useEffect 
+// useState
